aca6bd19d9863a1aca8b53d19fbdcc18d2c715a2
Julian Grisales <jgrisales@gmail.com>
2021-08-22 12:36:14 +0200

    Models

--- movierater/api/models.py
+++ movierater/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        # If the rating already exists (user, movie) will rejected
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
